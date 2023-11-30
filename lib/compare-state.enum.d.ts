ed46b6256d35f02469174f6eeea7398f19082bbc
Jérémy ACHAIN <achain.jeremy@gmail.com>
2023-11-30 21:44:08 +0100

    Improves

--- lib/compare-state.enum.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum CompareStateEnum {
-    NONE = "",
-    ADDED = "added",
-    MOVED = "moved",
-    UPDATED = "updated",
-    REMOVED = "removed",
-    EQUAL = "equal"
-}
-//# sourceMappingURL=compare-state.enum.d.ts.map
--- lib/finded-item.interface.d.ts
+++
@@ -1,6 +0,0 @@
-import { AnyValue } from '@alkemist/smart-tools';
-export interface FindedItemInterface {
-    index: number;
-    value: Record<string, AnyValue> | undefined;
-}
-//# sourceMappingURL=finded-item.interface.d.ts.map
